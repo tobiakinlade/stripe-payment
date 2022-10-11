927d742bcc904ad2a9b98f8f57c3466ef8cf1c3f
tobiakinlade <tobi.akinlade@yahoo.com>
2022-10-11 13:22:28 +0100

    stripe payment

---
+++ controllers/stripeController.js
@@ -0,0 +1,21 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+const stripeController = async (req, res) => {
+  const { purchase, total_amount, shipping_fee } = req.body
+
+  const calculateOrderAmount = () => {
+    return total_amount + shipping_fee
+  }
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(),
+    currency: 'usd',
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  })
+
+  res.json({ clientSecret: paymentIntent.client_secret })
+}
+
+module.exports = stripeController
